ad51d644a14d20d0d50b8d13020b27326ec01881
Alina Abibova <alinaten2103@gmail.com>
2026-04-18 11:11:34 +0500

    added calculation progress in courses

--- frontend/src/app/pages/courses/courses.ts
+++ frontend/src/app/pages/courses/courses.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CourseService } from '../../core/services/course';
+import { TaskService } from '../../core/services/task';
 
 @Component({
   selector: 'app-courses',
@@ -12,10 +13,15 @@ import { CourseService } from '../../core/services/course';
 export class CoursesComponent implements OnInit {
   newCourse = { name: '', description: '' };
   courses: any[] = [];
+  tasks: any[] = [];
   error: string = '';
   progressMap: { [key: number]: number } = {};
 
-  constructor(private courseService: CourseService, private cdr: ChangeDetectorRef) {}
+  constructor(
+    private courseService: CourseService,
+    private taskService: TaskService,
+    private cdr: ChangeDetectorRef
+  ) {}
 
   ngOnInit() {
     this.loadCourses();
@@ -26,10 +32,31 @@ export class CoursesComponent implements OnInit {
       next: (data: any[]) => {
         this.courses = data;
         this.error = '';
+      
+        this.taskService.getTasks().subscribe({
+          next: (tasks: any[]) => {
+            this.tasks = tasks;
+            this.calcProgress();
+            this.cdr.detectChanges();
+          },
+          error: () => {}
+        });
         this.cdr.detectChanges();
       },
       error: (err) => {
         this.error = 'Error loading courses: ' + err.status;
+      }
+    });
+  }
+
+  calcProgress() {
+    this.courses.forEach(course => {
+      const courseTasks = this.tasks.filter(t => t.course === course.id);
+      if (courseTasks.length === 0) {
+        this.progressMap[course.id] = 0;
+      } else {
+        const completed = courseTasks.filter(t => t.is_completed).length;
+        this.progressMap[course.id] = Math.round((completed / courseTasks.length) * 100);
       }
     });
   }
@@ -51,21 +78,12 @@ export class CoursesComponent implements OnInit {
 
   deleteCourse(id: number) {
     this.courseService.deleteCourse(id).subscribe({
-      next: () => this.loadCourses(),
-      error: () => this.error = 'Error deleting course'
-    });
-  }
-
-  updateProgress(id: number, value: number) {
-    this.courseService.updateProgress({
-      course: id,
-      percent: value
-    }).subscribe({
       next: () => {
-        this.progressMap[id] = value;
+        this.courses = this.courses.filter(c => c.id !== id);
+        delete this.progressMap[id];
         this.cdr.detectChanges();
       },
-      error: () => this.error = 'Progress error'
+      error: () => this.error = 'Error deleting course'
     });
   }
 }
